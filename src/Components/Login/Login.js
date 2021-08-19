1c3a76f7d4e4b76b288f9603497efe29be909bba
shourov-sorkar <18103142@iubat.edu>
2021-08-19 22:40:09 +0600

    :tada: Initial Commit

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import  "./Login.css"
+
+const Login = () => {
+    return (
+        <div>
+            <h1>Login Page works
+            </h1>
+        </div>
+    )
+}
+
+export default Login
