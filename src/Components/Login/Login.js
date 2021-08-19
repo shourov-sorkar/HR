6b13150cceef25e428bc640f4981218746bad766
shourov-sorkar <18103142@iubat.edu>
2021-08-20 00:52:08 +0600

    :triangular_flag_on_post: Added login page

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,11 +1,39 @@
 import React from 'react'
-import  "./Login.css"
-
+import "./Login.css"
+import logo from "../../Assets/logo.png"
 const Login = () => {
     return (
         <div>
-            <h1>Login Page works
-            </h1>
+            <div className="main__container">
+                <div className="color"></div>
+                <div className="color"></div>
+                <div className="color"></div>
+                <div className="box">
+                    <div className="square"></div>
+                    <div className="square" ></div>
+                    <div className="square" ></div>
+                    <div className="square" ></div>
+                    <div className="square" ></div>
+                    <div className="img"><img src={logo} alt="logo"  /></div>
+                    <div className="inner__container">
+                        <div className="form">
+                            <h2>Login</h2>
+                            <form>
+                                <div className="inputBox">
+                                    <input type="text" placeholder="Username" />
+                                </div>
+                                <div className="inputBox">
+                                    <input type="password" placeholder="Password" />
+                                </div>
+                                <div className="inputBox">
+                                    <input type="submit" value="Login" />
+                                </div>
+                                <p className="forget"> Forget Password? <a href="#!">Click Here</a></p>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
